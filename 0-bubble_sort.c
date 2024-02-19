46c830dd7cfcfafe917f6a6f89d1f660c23d27df
akinbode adeboyejo <akinbode.adeboyejo@gmail.com>
2024-02-19 08:49:29 -0800

    task 0

---
+++ 0-bubble_sort.c
@@ -0,0 +1,36 @@
+#include "sort.h"
+/**
+ * bubble_sort - sorts an array of integers in ascending order using the Bubble
+ * sort algorithm
+ *
+ * @array: the array to be sorted
+ * @size: size of the array
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j;
+	int swapped;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		for (j = 0; j < size - 1 - i; j++)
+		{
+			swapped = 0
+			if (array[j] > array[j + 1])
+			{
+				int temp = 0;
+
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				print_array(array, size);
+				swapped = 1;
+			}
+				if (swapped == 0)
+				{
+				break;
+			}		
+		}
+	}
+}
